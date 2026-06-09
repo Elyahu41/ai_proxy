55b8264ab5e563ce1c53ba531fd7f54249857e17
Ely <Elyahu41@gmail.com>
2026-06-09 00:09:31 -0400

    allow bigger images

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 const APP_SECRET = process.env.APP_SECRET || null;
 
